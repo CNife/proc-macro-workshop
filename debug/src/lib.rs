2933da1d1df78860be595dc81a99f1cbfd191880
CNife <CNife@vip.qq.com>
2022-01-13 00:40:12 +0800

    完成builder/01-parse

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,12 @@
-use proc_macro::TokenStream;
+use proc_macro::TokenStream as StdTokenStream;
+
+use proc_macro2::TokenStream;
+use syn::{DeriveInput, parse_macro_input};
 
 #[proc_macro_derive(CustomDebug)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+pub fn derive(input: StdTokenStream) -> StdTokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let _ = dbg!(input);
 
-    unimplemented!()
+    TokenStream::new().into()
 }
--- main.rs
+++ main.rs
@@ -1,9 +1,9 @@
-// Write code here.
-//
-// To see what the code looks like after macro expansion:
-//     $ cargo expand
-//
-// To run the code:
-//     $ cargo run
+use derive_debug::CustomDebug;
+
+#[derive(CustomDebug)]
+pub struct Field {
+    name: &'static str,
+    bitmask: u16,
+}
 
 fn main() {}
