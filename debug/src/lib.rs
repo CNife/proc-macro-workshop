4f1d160d1fb5256563147b4eb13be8d0fd33bc87
潭渊 <CNife@vip.qq.com>
2024-01-22 16:33:04 +0800

    完成debug:03-custom-format

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,26 +1,47 @@
 use proc_macro::TokenStream as StdTokenStream;
 
+use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields};
+use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Field, Fields, Meta, MetaNameValue};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: StdTokenStream) -> StdTokenStream {
     let DeriveInput { ident, data, .. } = dbg!(parse_macro_input!(input as DeriveInput));
-    let field_idents: Vec<_> = if let Data::Struct(DataStruct {
-        fields: Fields::Named(fields),
-        ..
-    }) = data
-    {
-        fields.named.into_iter().map(|f| f.ident.unwrap()).collect()
-    } else {
-        todo!()
+    let fields_code: TokenStream = match data {
+        Data::Struct(DataStruct {
+            fields: Fields::Named(fields),
+            ..
+        }) => fields
+            .named
+            .into_iter()
+            .map(|field| {
+                let Field { ident, attrs, .. } = field;
+                let field_ident = ident.unwrap();
+                let field_value_code = attrs
+                    .into_iter()
+                    .find_map(|attr| match attr.meta {
+                        Meta::NameValue(MetaNameValue { path, value, .. })
+                            if path.is_ident("debug") =>
+                        {
+                            Some(value)
+                        }
+                        _ => None,
+                    })
+                    .map_or_else(
+                        || quote!(&self.#field_ident),
+                        |format| quote!(&format_args!(#format, &self.#field_ident)),
+                    );
+                quote!(.field(stringify!(#field_ident), #field_value_code))
+            })
+            .collect(),
+        _ => quote!(),
     };
 
     let result = quote! {
         impl ::std::fmt::Debug for #ident {
             fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
                 f.debug_struct(stringify!(#ident))
-                #( .field(stringify!(#field_idents), &self.#field_idents) )*
+                #fields_code
                 .finish()
             }
         }
--- debug/src/main.rs
+++ debug/src/main.rs
@@ -3,13 +3,18 @@ use derive_debug::CustomDebug;
 #[derive(CustomDebug)]
 pub struct Field {
     name: &'static str,
+    #[debug = "0b{:08b}"]
     bitmask: u8,
 }
 
 fn main() {
-    let f = Field { name: "F", bitmask: 0b00011100 };
+    let f = Field {
+        name: "F",
+        bitmask: 0b00011100,
+    };
 
     let debug = format!("{:?}", f);
+    let expected = r#"Field { name: "F", bitmask: 0b00011100 }"#;
 
-    assert!(debug.starts_with(r#"Field { name: "F","#));
+    assert_eq!(debug, expected);
 }
