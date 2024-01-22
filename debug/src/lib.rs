66a775de92037bf6a4e45c3ee86245804af3738f
潭渊 <CNife@vip.qq.com>
2024-01-22 10:20:16 +0800

    完成debug:01-parse

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,11 @@
 use proc_macro::TokenStream;
 
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let tt = parse_macro_input!(input as DeriveInput);
+    eprintln!("{tt:#?}");
+    quote!().into()
 }
