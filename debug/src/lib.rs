cb53f542ddab3c8ef63c5469e2ecf5004c1a6fe8
潭渊 <CNife@vip.qq.com>
2024-01-22 14:48:37 +0800

    完成debug:02-impl-debug

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,11 +1,31 @@
-use proc_macro::TokenStream;
+use proc_macro::TokenStream as StdTokenStream;
 
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields};
 
 #[proc_macro_derive(CustomDebug)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let tt = parse_macro_input!(input as DeriveInput);
-    eprintln!("{tt:#?}");
-    quote!().into()
+pub fn derive(input: StdTokenStream) -> StdTokenStream {
+    let DeriveInput { ident, data, .. } = dbg!(parse_macro_input!(input as DeriveInput));
+    let field_idents: Vec<_> = if let Data::Struct(DataStruct {
+        fields: Fields::Named(fields),
+        ..
+    }) = data
+    {
+        fields.named.into_iter().map(|f| f.ident.unwrap()).collect()
+    } else {
+        todo!()
+    };
+
+    let result = quote! {
+        impl ::std::fmt::Debug for #ident {
+            fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+                f.debug_struct(stringify!(#ident))
+                #( .field(stringify!(#field_idents), &self.#field_idents) )*
+                .finish()
+            }
+        }
+    };
+
+    eprintln!("{result}");
+    result.into()
 }
