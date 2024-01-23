4822d7307af291d5c8e58ffe040c153a15231363
潭渊 <CNife@vip.qq.com>
2024-01-23 15:53:40 +0800

    完成builder的01-parse

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,19 @@
-use proc_macro::TokenStream;
+use proc_macro::TokenStream as StdTokenStream;
+
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+pub fn derive(input: StdTokenStream) -> StdTokenStream {
+    let derive_input = parse_macro_input!(input as DeriveInput);
+    let result = make_derive_builder(dbg!(derive_input)).unwrap_or_else(|e| e.to_compile_error());
+    eprintln!("{result}");
+    result.into()
+}
 
-    unimplemented!()
+type TokenStreamResult = syn::Result<TokenStream>;
+
+fn make_derive_builder(input: DeriveInput) -> TokenStreamResult {
+    Ok(quote! {})
 }
